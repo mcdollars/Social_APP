fea1f80510e9ce12ca5a99fc52bcaeeee3d423b3
FateNinja <fateninja9376@gmail.com>
2022-08-23 22:52:58 +0800

    added landing page

--- src/pages/CreateExperienceMap.tsx
+++ src/pages/CreateExperienceMap.tsx
@@ -29,6 +29,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { add, camera, bookmark, star } from "ionicons/icons";
 import "./CreateExperienceMap.scss";
 
+import { isPlatform } from "@ionic/react";
+
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
+import { Filesystem, Directory } from "@capacitor/filesystem";
+import { Storage } from "@capacitor/storage";
+import { Capacitor } from "@capacitor/core";
+
+const PHOTO_STORAGE = "photos";
+
 interface OwnProps {}
 
 interface StateProps {
@@ -38,7 +52,12 @@ interface StateProps {
 
 interface DispatchProps {}
 
-interface GroupsProps extends OwnProps, StateProps, DispatchProps {}
+interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
+interface GroupsProps extends OwnProps, StateProps, DispatchProps, UserPhoto {}
 
 const Groups: React.FC<GroupsProps> = ({ speakers, speakerSessions }) => {
   let leafletMap: any;
@@ -54,6 +73,77 @@ const Groups: React.FC<GroupsProps> = ({ speakers, speakerSessions }) => {
     if (router.canGoBack()) {
       router.goBack();
     }
+  };
+
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === "string") {
+          resolve(reader.result);
+        } else {
+          reject("method did not return a string");
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    let base64Data: string;
+    // "hybrid" will detect Cordova or Capacitor;
+    if (isPlatform("hybrid")) {
+      const file = await Filesystem.readFile({
+        path: photo.path!,
+      });
+      base64Data = file.data;
+    } else {
+      base64Data = await base64FromPath(photo.webPath!);
+    }
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    if (isPlatform("hybrid")) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    }
+  };
+
+  const takePhoto = async () => {
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    const fileName = new Date().getTime() + ".jpeg";
+    const savedFileImage = await savePicture(photo, fileName);
+    const newPhotos = [savedFileImage, ...photos];
+    setPhotos(newPhotos);
+    Storage.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
+    console.log({ photo, newPhoto: JSON.stringify(newPhotos) });
   };
 
   const loadMap = () => {
@@ -118,7 +208,11 @@ const Groups: React.FC<GroupsProps> = ({ speakers, speakerSessions }) => {
                   <IonIcon icon={add} />
                 </IonFabButton>
                 <IonFabList side="top">
-                  <IonFabButton color="primary" title="Add Photo">
+                  <IonFabButton
+                    color="primary"
+                    title="Add Photo"
+                    onClick={takePhoto}
+                  >
                     <IonIcon icon={camera} />
                   </IonFabButton>
                   <IonFabButton color="secondary" title="Saved Places">
--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,58 +1,204 @@
-import React, { useState } from 'react';
-import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, useIonViewWillEnter } from '@ionic/react';
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  useIonViewWillEnter,
+} from "@ionic/react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-import { Swiper as SwiperCore } from 'swiper';
-import { arrowForward } from 'ionicons/icons';
-import { setMenuEnabled } from '../data/sessions/sessions.actions';
-import { setHasSeenTutorial } from '../data/user/user.actions';
-import './Tutorial.scss';
-import 'swiper/swiper.min.css';
-import '@ionic/react/css/ionic-swiper.css';
-import { connect } from '../data/connect';
-import { RouteComponentProps } from 'react-router';
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import { Swiper as SwiperCore } from "swiper";
+import { arrowForward } from "ionicons/icons";
+import { setMenuEnabled } from "../data/sessions/sessions.actions";
+import { setHasSeenTutorial } from "../data/user/user.actions";
+import "./Tutorial.scss";
+import "swiper/swiper.min.css";
+import "@ionic/react/css/ionic-swiper.css";
+import { connect } from "../data/connect";
+import { RouteComponentProps } from "react-router";
 
-interface OwnProps extends RouteComponentProps {};
+interface OwnProps extends RouteComponentProps {}
 
 interface DispatchProps {
   setHasSeenTutorial: typeof setHasSeenTutorial;
   setMenuEnabled: typeof setMenuEnabled;
 }
 
-interface TutorialProps extends OwnProps, DispatchProps { };
+interface TutorialProps extends OwnProps, DispatchProps {}
 
-const Tutorial: React.FC<TutorialProps> = ({ history, setHasSeenTutorial, setMenuEnabled }) => {
+const Tutorial: React.FC<TutorialProps> = ({
+  history,
+  setHasSeenTutorial,
+  setMenuEnabled,
+}) => {
   const [showSkip, setShowSkip] = useState(true);
   let [swiper, setSwiper] = useState<SwiperCore>();
 
   useIonViewWillEnter(() => {
     setMenuEnabled(false);
   });
-  
-  const startApp = async () => { 
+
+  const startApp = async () => {
     await setHasSeenTutorial(true);
     await setMenuEnabled(true);
-    history.push('/tabs/home', { direction: 'none' });
+    history.push("/tabs/home", { direction: "none" });
   };
 
-  const handleSlideChangeStart = () => { 
-    if(!swiper) return;
+  const handleSlideChangeStart = () => {
+    if (!swiper) return;
     setShowSkip(!swiper.isEnd);
   };
 
   return (
     <IonPage id="tutorial-page">
-      <IonHeader no-border>
-        <IonToolbar>
-          <IonButtons slot="end">
-            {showSkip && <IonButton color='primary' onClick={startApp}>Skip</IonButton>}
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
+        <div>
+          <div className="content-onboarding">
+            <div className="relative">
+              <div className="holder-images grid grid-cols-3 gap-px">
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-285.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-286.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-287.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-288.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-289.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-290.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-291.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-292.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-284.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-293.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-294.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+                <div className="image-holder w-full h-full">
+                  <img
+                    src="assets/images/onboarding/Rectangle-295.png"
+                    className="w-full h-full"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="absolute bottom-content bottom-0 translate-y-1/2 w-32 h-32 left-1/2 -translate-x-1/2">
+                <div className="circle rounded-full flex items-center justify-center bg-transparent border-2 w-full h-full">
+                  <div className="image-holder bg-main-color rounded-full w-4/5 h-4/5 border-8 border-white shadow-xl flex items-center justify-center">
+                    <img
+                      className="w-1/3"
+                      src="assets/images/short-light-logo.png"
+                      alt="short logo"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="img-7-holder absolute top-1/4 -translate-y-1/2 left-1/3 -translate-x-1/2">
+                <img src="assets/images/onboarding/7.png" alt="" />
+              </div>
+              <div className="img-8-holder absolute right-1/4 translate-x-3/4 top-1/4 -translate-y-full">
+                <img src="assets/images/onboarding/8.png" alt="" />
+              </div>
+              <div className="img-3-holder absolute right-1/4 translate-x-2 top-1/4 translate-y-1/3">
+                <img src="assets/images/onboarding/3.png" alt="" />
+              </div>
+              <div className="img-6-holder absolute top-1/2 -translate-y-1/4 right-1/2 -translate-x-full">
+                <img src="assets/images/onboarding/6.png" alt="" />
+              </div>
+              <div className="img-4-holder absolute bottom-1/3 translate-y-1/2 right-1/2 translate-x-1/2">
+                <img src="assets/images/onboarding/4.png" alt="" />
+              </div>
+              <div className="img-2-holder absolute right-1/4 translate-x-full bottom-2/4 translate-y-full">
+                <img src="assets/images/onboarding/2.png" alt="" />
+              </div>
+              <div className="img-1-holder absolute left-1/4 -translate-x-full bottom-1/4 translate-y-full">
+                <img src="assets/images/onboarding/1.png" alt="" />
+              </div>
+              <div className="img-5-holder absolute right-1/4 translate-x-3/4 bottom-1/4 translate-y-full">
+                <img src="assets/images/onboarding/5.png" alt="" />
+              </div>
+            </div>
+            <div className="info-content text-center">
+              <div className="title text-main-color mt-20 text-3xl font-medium leading-7 mb-4">
+                Share the experience
+              </div>
+              <div className="info secondary-color w-2/3 mx-auto mb-8">
+                Find out the best places to go from people you know
+              </div>
+              <div className="button-holder mb-8 mx-auto" onClick={startApp}>
+                <span className="get-started bg-main-color text-white w-5/6 inline-block rounded-xl py-3 font-medium">
+                  Get started
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
 
-        <Swiper onSwiper={setSwiper} onSlideChangeTransitionStart={handleSlideChangeStart}>
+        {/* <Swiper onSwiper={setSwiper} onSlideChangeTransitionStart={handleSlideChangeStart}>
           <SwiperSlide>
             <img src="assets/img/ica-slidebox-img-1.png" alt="" className="slide-image" />
             <h2 className="slide-title">
@@ -87,16 +233,16 @@ const Tutorial: React.FC<TutorialProps> = ({ history, setHasSeenTutorial, setMen
               <IonIcon slot="end" icon={arrowForward} />
             </IonButton>
           </SwiperSlide>
-        </Swiper>
+        </Swiper> */}
       </IonContent>
     </IonPage>
   );
 };
 
 export default connect<OwnProps, {}, DispatchProps>({
-  mapDispatchToProps: ({
+  mapDispatchToProps: {
     setHasSeenTutorial,
-    setMenuEnabled
-  }),
-  component: Tutorial
+    setMenuEnabled,
+  },
+  component: Tutorial,
 });
